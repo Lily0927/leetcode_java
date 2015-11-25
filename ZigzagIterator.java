528e447ce79e7a9a2badc44103a8ecfafc100f55
Lily <120211433@msn.cn>
2015-11-24 21:06:33 -0700

    add zigzag iterator and three sum smaller

--- ZigzagIterator.java
+++ ZigzagIterator.java
@@ -1,5 +1,33 @@
 package Leetcode;
 
-public class ZigzagIterator {
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 
+public class ZigzagIterator {
+	LinkedList<Iterator> list;
+	public ZigzagIterator(List<Integer> v1, List<Integer> v2) {
+		list=new LinkedList<Iterator>();
+		if(v1.size()>0){
+			list.add(v1.iterator());
+		}
+		if(v2.size()>0){
+			list.add(v2.iterator());
+		}
+	}
+
+	public int next() {
+		Iterator<Integer> iter=list.remove();
+		int value=iter.next();
+		if(iter.hasNext()){
+			list.add(iter);
+			
+		}
+		return value;
+
+	}
+
+	public boolean hasNext() {
+		return !list.isEmpty();
+	}
 }
