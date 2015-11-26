d22df4f6df96d2c72cf649ba2a002986d8f55dc6
Lily <120211433@msn.cn>
2015-11-26 16:54:31 -0700

    add fraction to recurring decimal

---
+++ FractiontoRecurringDecimal.java
@@ -0,0 +1,47 @@
+package Leetcode;
+
+import java.util.HashMap;
+
+public class FractiontoRecurringDecimal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(fractionToDecimal(-1,-2147483648));
+	}
+
+	public static String fractionToDecimal(int numerator, int denominator) {
+		
+		if(numerator==0){
+			return "0";
+		}
+		StringBuilder result=new StringBuilder();
+		result.append(((numerator>0)^(denominator>0))?"-":"");
+		
+		long num=Math.abs((long)numerator);
+		long deno=Math.abs((long)denominator);
+		
+		result.append(num/deno);
+		num=num%deno;
+		if(num==0){
+			return result.toString();
+		}
+		result.append(".");
+		HashMap<Long,Integer> store=new HashMap<Long,Integer>();
+		store.put(num, result.length());
+		while(num!=0){
+			num*=10;
+			result.append(num/deno);
+			num%=deno;
+			if(store.containsKey(num)){
+				int index=store.get(num);
+				result.insert(index, "(");
+				result.append(")");
+				break;
+			}
+			else{
+				store.put(num, result.length());
+			}			
+		}
+		return result.toString();		
+	}
+}
--- MeetingRoomII.java
+++ MeetingRoomII.java
@@ -1,10 +1,54 @@
 package Leetcode;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 public class MeetingRoomII {
+	public class Interval {
+		int start;
+		int end;
+
+		Interval() {
+			start = 0;
+			end = 0;
+		}
+
+		Interval(int s, int e) {
+			start = s;
+			end = e;
+		}
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	}
+
+	public int minMeetingRooms(Interval[] intervals) {
+		if(intervals==null || intervals.length==0){
+			return 0;
+		}
+		int result=1;
+		PriorityQueue<Integer> pq=new PriorityQueue<Integer>();
+		Arrays.sort(intervals, new Comparator<Interval>(){
+
+			@Override
+			public int compare(Interval o1, Interval o2) {
+				// TODO Auto-generated method stub
+				return o1.start-o2.start;
+			}});
+		pq.add(intervals[0].end);
+		for(int i=1;i<intervals.length;i++){
+			if(intervals[i].start<pq.peek()){
+				result++;			
+			}
+			else{
+				pq.poll();				
+			}
+			pq.add(intervals[i].end);
+		}
+		return result;
 	}
 
 }
