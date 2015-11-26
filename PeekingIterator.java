73340f4c40a427e5c60031f38d8ddd2fce15ee73
Lily <120211433@msn.cn>
2015-11-25 22:37:29 -0700

    add meeting room 2

--- PeekingIterator.java
+++ PeekingIterator.java
@@ -2,26 +2,37 @@ package Leetcode;
 
 import java.util.Iterator;
 
-public class PeekingIterator implements Iterator<Integer> {
+public class PeekingIterator  {
+	private Integer next;
+	private Iterator<Integer> iter;
 	public PeekingIterator(Iterator<Integer> iterator) {
 	    // initialize any member here.
-	    
+		iter=iterator;
+		if(iter.hasNext()){
+			next=iter.next();
+		}
+		    
 	}
 
     // Returns the next element in the iteration without advancing the iterator.
 	public Integer peek() {
-        
+        return next;
 	}
 
 	// hasNext() and next() should behave the same as in the Iterator interface.
 	// Override them if needed.
-	@Override
 	public Integer next() {
-	    
+	    Integer curr=next;
+	    if(iter.hasNext()){
+	    	next=iter.next();
+	    }
+	    else{
+	    	next=null;
+	    }
+	    return curr;
 	}
 
-	@Override
 	public boolean hasNext() {
-	    
+	    return next!=null;
 	}
 }
